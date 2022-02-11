493502af9b4c63c1812a948667f8143a58b7f46f
Marcel Eichner <love@ephigenia.de>
2022-02-11 19:41:47 +0100

    test: wip wip

--- source/lib/option-parser.test.ts
+++ source/lib/option-parser.test.ts
@@ -2,6 +2,36 @@ import { expect } from 'chai';
 import { InvalidOptionArgumentError } from 'commander';
 import * as lib from './option-parser';
 
+
+describe('parseIban', function () {
+  const valid = [
+    'DE86701500000094203609',
+    'DE867015000000942',
+  ];
+  valid.forEach(iban => {
+    it(`valid ${iban}`, function() {
+      expect(lib.parseIban(iban)).to.be.a('String');
+    });
+  });
+
+  describe('invalid', function() {
+    [
+      1,
+      '1',
+      'DE12',
+      'DE',
+      'DE86 7015 0000 0094 2036 09',
+      ' DE86701500000094203609',
+    ].forEach((input) => {
+      it(`${JSON.stringify(input)}`, function () {
+        expect(() => lib.parseIban(input)).to.throw(
+          InvalidOptionArgumentError,
+        );
+      });
+    });
+  });
+}); // parseIban
+
 describe('parseDateAndTime', function () {
   const valid = [
     ['2022-01-01', '2022-01-01T00:00:00.000Z'],
@@ -37,8 +67,24 @@ describe('parseDateAndTime', function () {
 }); // suite
 
 describe('parseAmount', function () {
-  it('doesnt accept it', function () {
-    const f = lib.parseAmount('12381');
-    expect(f).to.equal(12381);
+  const valid = [
+    ['1', 1],
+    ['0', 0],
+    ['12381', 12381],
+    ['109', 109],
+  ];
+  valid.forEach(([input, result]) => {
+    it(`${JSON.stringify(input)} to ${JSON.stringify(result)}`, function () {
+      expect(lib.parseAmount(input)).to.equal(result);
+    });
+  });
+
+  const invalid = ['-1', false, {}, ' 1', ' 1 ', '1 ', '2.1', '2.', '2.3'];
+  invalid.forEach((value) => {
+    it(`doesn’t accept ${JSON.stringify(value)}`, function () {
+      expect(() => lib.parseAmount(value)).to.throw(
+        InvalidOptionArgumentError,
+      );
+    });
   });
 });
--- source/lib/option-parser.ts
+++ source/lib/option-parser.ts
@@ -1,6 +1,6 @@
 import { InvalidArgumentError } from 'commander';
 
-export function parseDateAndTime(value: string) {
+export function parseDateAndTime(value: string) : Date {
   if (!/^20\d\d-\d\d-\d\d$/.test(value)) {
     throw new InvalidArgumentError('unable to parse date');
   }
@@ -11,13 +11,17 @@ export function parseDateAndTime(value: string) {
   return new Date(timestamp);
 }
 
-export function parseIban(str: string) {
+export function parseIban(str: unknown) {
+  const regexp = /^[A-Z]{2}[0-9]{2}\d+$/;
+  if (!regexp.test(String(str))) {
+    throw new InvalidArgumentError('unable to parse iban');
+  }
   return str;
 }
 
-export function parseAmount(str: string) {
-  if (!/^\d+$/.test(str)) {
-    throw new InvalidArgumentError('unable to parse date');
+export function parseAmount(str: unknown) : number {
+  if (!/^\d+$/.test(String(str))) {
+    throw new InvalidArgumentError('unable to parse amount');
   }
   return +str;
 }
